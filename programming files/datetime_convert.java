471e2b3223651bbc2d552b0c369eec9d6e30c08d
Penofcilin <63890812+penofcilin@users.noreply.github.com>
2025-04-01 16:56:10 -0400

    Update datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,11 +3,16 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
+        //creating date string
         String dateStr = "2022-03-17 10:45:30";
+        //formatting the date tiem with the pattern yyyy-MM-dd HH:mm:ss
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        //Getting the local date time
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
+        //formatting the localdate time
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
+        //outputting the formatted date
         System.out.println(formattedDate);
     }
 }
