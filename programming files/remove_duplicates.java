fb6eee9921e7414f4861d5b01d62eeb186279d80
Driedupisaac <98975199+Driedupisaac@users.noreply.github.com>
2025-04-01 16:53:34 -0400

    remove_duplicates.java is bugging

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class remove_duplicates {
 
-    public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
+    public static List<Integer> removeduplicates(List<Integer> arr, List<Integer> removedElements) {
         List<Integer> uniqueArr = new ArrayList<>();
         for (int i = 0; i < arr.size(); i++) {
             if (!uniqueArr.contains(arr.get(i))) {
-                uniqueArr.add(arr.get(i));
+                uniqueArr.add(Double.toD(arr.get(i)));
             } else {
-                removedElements.add(arr.get(i));
+                removedElements.add(arr.get(i+1));
             }
         }
         return uniqueArr;
@@ -18,7 +18,7 @@ public class remove_duplicates {
 
     public static void main(String[] args) {
         // Test the function
-        List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
+        List<Integer> arr = Arrays.asList(1, 2, 2, "3", 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
         List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
         System.out.println("Original array: " + arr);
